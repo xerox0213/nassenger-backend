3dc1e0edc8f096a5790b99ef104c47b7aa7ad31f
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-09 12:50:48 +0100

    feat: inscription d'un utilisateur

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegisterRequest;
 use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
@@ -13,5 +15,17 @@ class AuthController extends Controller
             'success' => true,
             'data' => new UserResource($request->user())
         ]);
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        $credentials = $request->validated();
+
+        User::create($credentials);
+
+        return response()->json([
+            'success' => true,
+            'data' => null
+        ], 201);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', [AuthController::class, 'user'])
     ->name('auth.user')
     ->middleware('auth:sanctum');
+
+Route::post('/register', [AuthController::class, 'register'])
+    ->name('auth.register');
