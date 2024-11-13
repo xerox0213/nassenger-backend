e598e69a47d31fdf58e170b1c6a26494ab5d4021
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-13 13:19:38 +0100

    feat: modification rendu ValidationException
    Permet d'uniformiser les réponses json même en cas d'erreur

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Helpers\ApiResponseHelper;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +18,13 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+
+        $exceptions->render(function (ValidationException $e, Request $request) {
+            $message = $e->getMessage();
+            $errors = $e->errors();
+            $status = $e->status;
+
+            return ApiResponseHelper::jsonError($message, $errors, $status);
+        });
+
     })->create();
