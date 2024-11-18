47a56e0382b5c1d493afc9e12d29899ef940d655
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-18 13:50:57 +0100

    test: recherche membres par morceau de prénom

--- tests/Feature/MemberApiTest.php
+++ tests/Feature/MemberApiTest.php
@@ -26,4 +26,20 @@ class MemberApiTest extends TestCase
                 ->has('data', 3)
                 ->etc());
     }
+
+    public function test_should_filter_by_piece_of_firstname()
+    {
+        $me = User::factory()->create(['firstname' => 'John', 'lastname' => 'Doe']);
+        User::factory()->create(['firstname' => 'Elena', 'lastname' => 'Gilbert']);
+        User::factory()->create(['firstname' => 'Elenor', 'lastname' => 'Mino']);
+        User::factory()->create(['firstname' => 'Eleana', 'lastname' => 'Ono']);
+
+        $response = $this->actingAs($me)->getJson(route('members.index', ['full_name' => 'len']));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn(AssertableJson $json) => $json
+                ->has('data', 2)
+                ->etc());
+    }
 }
