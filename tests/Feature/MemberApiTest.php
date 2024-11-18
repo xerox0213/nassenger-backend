0a1d72bd4f196d71551880631f825354901c6004
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-18 13:54:22 +0100

    test: recherche membre par nom et prénom

--- tests/Feature/MemberApiTest.php
+++ tests/Feature/MemberApiTest.php
@@ -76,4 +76,21 @@ class MemberApiTest extends TestCase
                 ->has('data', 3)
                 ->etc());
     }
+
+    public function test_should_filter_by_firstname_and_lastname()
+    {
+        $me = User::factory()->create(['firstname' => 'John', 'lastname' => 'Doe']);
+        User::factory()->create(['firstname' => 'Stefan', 'lastname' => 'Salvatore']);
+        User::factory()->create(['firstname' => 'Stefan', 'lastname' => 'Salomon']);
+        User::factory()->create(['firstname' => 'Stefan', 'lastname' => 'Saloran']);
+        User::factory()->create(['firstname' => 'Stefan', 'lastname' => 'Situmian']);
+
+        $response = $this->actingAs($me)->getJson(route('members.index', ['full_name' => "Stefan Salvatore"]));
+
+        $response
+            ->assertStatus(200)
+            ->assertJson(fn(AssertableJson $json) => $json
+                ->has('data', 1)
+                ->etc());
+    }
 }
