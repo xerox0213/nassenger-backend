aabc53fb8c5c7f0508d51cfae3b32b290430bffb
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-20 14:51:06 +0100

    feat: création MessageAuthorResource
    Formatte les données d'un utilisateur pour garder uniquement les données nécessaire et suffisante de l'auteur d'un message.

---
+++ app/Http/Resources/MessageAuthorResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin User */
+class MessageAuthorResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'firstname' => $this->firstname,
+            'lastname' => $this->lastname,
+            'avatar' => $this->avatar,
+            'you' => $request->user()->id == $this->id
+        ];
+    }
+}
