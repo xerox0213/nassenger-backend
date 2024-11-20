dd934c6f24dad672fda2863b24ed91cf1c2aefbd
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-20 20:26:13 +0100

    test: création message avec message initial

--- tests/Feature/ConversationMessageStoreApiTest.php
+++ tests/Feature/ConversationMessageStoreApiTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use App\Enums\MessageType;
 use App\Models\Conversation;
+use App\Models\Message;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Testing\Fluent\AssertableJson;
@@ -40,6 +41,42 @@ class ConversationMessageStoreApiTest extends TestCase
                     ->where('author.avatar', $me->avatar)
                     ->where('author.you', true)
                     ->where('initial_message', null)
+                    ->etc())
+            );
+    }
+
+    public function test_should_store_the_message_in_the_conversation_with_initial_message_id()
+    {
+        $me = User::factory()->create();
+        $contact = User::factory()->create();
+
+        $conversation = Conversation::factory()->hasAttached(collect([$me, $contact]), ['is_admin' => false])->create();
+
+        $initialMessage = Message::factory()->for($contact)->for($conversation)->create();
+
+        $messageContent = 'Goose - Synrise';
+
+        $response = $this->actingAs($me)->postJson(route('conversations.messages.store', [
+            'conversation' => $conversation->id
+        ]), [
+            'content' => $messageContent,
+            'initial_message_id' => $initialMessage->id
+        ]);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(fn(AssertableJson $json) => $json
+                ->has('data', fn(AssertableJson $json) => $json
+                    ->where('type', MessageType::TEXT)
+                    ->where('content', $messageContent)
+                    ->where('author.id', $me->id)
+                    ->where('author.firstname', $me->firstname)
+                    ->where('author.lastname', $me->lastname)
+                    ->where('author.avatar', $me->avatar)
+                    ->where('author.you', true)
+                    ->where('initial_message.id', $initialMessage->id)
+                    ->where('initial_message.content', $initialMessage->content)
+                    ->where('initial_message.type', $initialMessage->type)
                     ->etc())
             );
     }
