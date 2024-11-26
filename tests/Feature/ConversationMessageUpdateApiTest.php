24f8370a75262d423fe9db7a3537158c1c02b83a
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-26 11:53:44 +0100

    test: mise à jour contenu d'un message

---
+++ tests/Feature/ConversationMessageUpdateApiTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Conversation;
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ConversationMessageUpdateApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_update_the_message_content()
+    {
+        $me = User::factory()->create();
+        $contact = User::factory()->create();
+        $conversation = Conversation::factory()->hasAttached(collect([$me, $contact]), ['is_admin' => false])->create();
+        $message = Message::factory()->for($me)->for($conversation)->create();
+        $newMessageContent = 'Once upon a time.';
+
+        $response = $this->actingAs($me)->patchJson(route('conversations.messages.update',[
+            'conversation' => $conversation->id,
+            'message' => $message->id
+        ]), ['content' => $newMessageContent]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonPath('data.id', $message->id)
+            ->assertJsonPath('data.content', $newMessageContent);
+    }
+}
