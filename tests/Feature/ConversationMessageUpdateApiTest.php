f2e51213b95d7f8225bde77e3a0954dbeaf34b8b
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-26 11:54:39 +0100

    test: échec màj contenu message si nouveau contenu pas présent

--- tests/Feature/ConversationMessageUpdateApiTest.php
+++ tests/Feature/ConversationMessageUpdateApiTest.php
@@ -30,4 +30,19 @@ class ConversationMessageUpdateApiTest extends TestCase
             ->assertJsonPath('data.id', $message->id)
             ->assertJsonPath('data.content', $newMessageContent);
     }
+
+    public function test_should_fail_if_the_new_message_content_is_not_present()
+    {
+        $me = User::factory()->create();
+        $contact = User::factory()->create();
+        $conversation = Conversation::factory()->hasAttached(collect([$me, $contact]), ['is_admin' => false])->create();
+        $message = Message::factory()->for($me)->for($conversation)->create();
+
+        $response = $this->actingAs($me)->patchJson(route('conversations.messages.update',[
+            'conversation' => $conversation->id,
+            'message' => $message->id
+        ]));
+
+        $response->assertInvalid('content');
+    }
 }
