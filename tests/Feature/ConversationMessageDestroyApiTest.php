2596635da5c098e1580304e7a045c4ed618e3fa1
Nasreddine Boutouil <nasreddine021@gmail.com>
2024-11-27 15:03:00 +0100

    test: suppression d'un message d'une conversation

---
+++ tests/Feature/ConversationMessageDestroyApiTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Conversation;
+use App\Models\Message;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ConversationMessageDestroyApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_should_delete_the_message()
+    {
+        $me = User::factory()->create();
+        $contact = User::factory()->create();
+        $conversation = Conversation::factory()->hasAttached(collect([$me, $contact]), ['is_admin' => false])->create();
+        $message = Message::factory()->for($me)->for($conversation)->create();
+
+        $response = $this->actingAs($me)->delete(route('conversations.messages.destroy', [
+            'conversation' => $conversation->id,
+            'message' => $message->id
+        ]));
+
+        $response->assertStatus(204);
+        $this->assertModelMissing($message);
+    }
+}
