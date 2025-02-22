3ba08027d54398570e5e310d96a352c9f66ddbc3
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-02-22 17:41:38 +0100

    fix(auth): use Hash facade for password encryption in registration

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,12 +7,13 @@ use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request) {
         $credentials = $request->validated();
-        $credentials['password'] = bcrypt($credentials['password']);
+        $credentials['password'] = Hash::make($credentials['password']);
         User::create($credentials);
         return response()->noContent();
     }
