339428d40ceb680ec5d76f140c03740d32078725
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-02-22 17:03:28 +0100

    feat(api): add route name

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register', [AuthController::class, 'register'])->name('auth.register');
 
-Route::post('/login', [AuthController::class, 'login']);
+Route::post('/login', [AuthController::class, 'login'])->name('auth.login');
 
-Route::delete('/logout', [AuthController::class, 'logout']);
+Route::delete('/logout', [AuthController::class, 'logout'])->name('auth.logout');
